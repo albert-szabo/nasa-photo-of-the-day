435ef10f8d02bb8e63c6999a53f90826887fc549
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-04-21 15:19:12 -0700

    Added styling via styled components

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const currentPhoto = (props) => {
     return (
-        <div className='photo-wrapper'>
+        <StyledPhoto>
             <h2>{props.photo.title}</h2>
             <h5>{props.photo.date}</h5>
             <img src={props.photo.hdurl} />
             <p className='description'>{props.photo.explanation}</p>
-        </div>
+        </StyledPhoto>
     )
 }
+
+const StyledPhoto = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 100vw;
+    border: 5px solid greenyellow;
+
+    background-color: ${props => props.theme.black};
+    color: ${props => props.theme.white};
+
+    transition: all .3s ease-in-out;
+    &:hover {
+        background-color: ${props => props.theme.primaryColor};
+        transition: all .3s ease-in-out;
+    }
+
+    p {
+        color: ${props => props.theme.secondaryColor};
+    }
+
+    h2 {
+        color: ${props => props.theme.primaryColor};
+    }
+
+    h5 {
+        color: ${props => props.theme.tertiaryColor};
+    }
+
+    img {
+        max-width: 95vw;
+    }
+
+    .description {
+        max-width: 75vw;
+    }
+`
 
 export default currentPhoto;
