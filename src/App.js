80178b86d1487f28ba4b67ebffbd5121cbdfd578
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-04-20 17:47:38 -0700

    Completed and tested photo display functionality

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 
+import CurrentPhoto from "./Components/Photo";
+
 function App() {
   const [photo, setPhoto] = useState();
 
   useEffect(() => {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then(response => {
-        console.log(response);
+        setPhoto(response.data);
       })
       .catch(error => console.error(error))
   }, [])
-  
+
   return (
     <div className="App">
-
+      { photo && <CurrentPhoto photo={photo} /> }
     </div>
   );
 }
