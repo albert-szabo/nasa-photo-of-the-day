de922c754e53f69843a4d6d3a8350675d09c0db2
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-04-20 17:22:21 -0700

    Successfully received data via API call

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [photo, setPhoto] = useState();
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.error(error))
+  }, [])
+  
   return (
     <div className="App">
 
